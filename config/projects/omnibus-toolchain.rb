2df4bee94eb51d184e20016aa9748f7b452a36dc
Seth Chisamore <schisamo@chef.io>
2015-12-10 21:21:13 -0500

    Bump Git version to 2.6.2
    This matches what we currently deploy on non-toolchain
    platforms via the Omnibus cookbook:
    https://github.com/chef-cookbooks/omnibus/blob/v3.1.3/attributes/default.rb#L24

--- config/projects/omnibus-toolchain.rb
+++ config/projects/omnibus-toolchain.rb
@@ -26,7 +26,7 @@ build_iteration 1
 
 override :ruby, version: "2.1.5"
 override :zlib, version: "1.2.8"
-override :git,  version: "1.9.0"
+override :git,  version: "2.6.2"
 override :cacerts, version: '2014.08.20'
 
 # creates required build directories
