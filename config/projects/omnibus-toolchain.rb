9502e2de787f8f1632770e0fa3e282042d1e1fc2
Scott Hain <shain@chef.io>
2016-04-22 14:03:04 -0700

    Override version

--- config/projects/omnibus-toolchain.rb
+++ config/projects/omnibus-toolchain.rb
@@ -23,7 +23,7 @@ install_dir    '/opt/omnibus-toolchain'
 build_version   '1.1.6'
 build_iteration 1
 
-override :ruby, version: "2.1.6"
+override :ruby, version: "2.3.0"
 override :git,  version: "2.7.3"
 
 # creates required build directories
