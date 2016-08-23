4f00a1905271d8fc431987cbc5b3f1dac31b6e37
Seth Chisamore <schisamo@chef.io>
2016-08-23 10:01:24 -0700

    Add build cookbook deps for various ES cookbooks
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- .delivery/build_cookbook/metadata.rb
+++ .delivery/build_cookbook/metadata.rb
@@ -3,3 +3,10 @@ maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
 version '0.1.0'
+
+# Engineering Services cookbooks (will eventually be deps of `delivery-bus`)
+depends 'artifactory-pro'
+depends 'java'
+depends 'jenkins'
+depends 'oc-artifactory'
+depends 'opscode-ci'
