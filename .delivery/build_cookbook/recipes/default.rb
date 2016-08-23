69dc128bdc7195e6f48ccbbb7364bd445074cdaa
Seth Chisamore <schisamo@chef.io>
2016-08-23 10:04:10 -0700

    Update publish recipe to use various ES primitives
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- .delivery/build_cookbook/recipes/default.rb
+++ .delivery/build_cookbook/recipes/default.rb
@@ -3,3 +3,6 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# Ensure Java is installed for remote triggering of Jenkins jobs with `jenkins_job`
+include_recipe 'java::default'
--- .delivery/build_cookbook/recipes/publish.rb
+++ .delivery/build_cookbook/recipes/publish.rb
@@ -3,3 +3,30 @@
 # Recipe:: publish
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+#########################################################################
+# TODO: set these things in `delivery-bus`
+#########################################################################
+delivery_bus_secrets = DeliverySugar::ChefServer.new.encrypted_data_bag_item('delivery-bus', 'secrets')
+
+node.set['jenkins']['master']['endpoint']  = 'http://manhattan.ci.chef.co'
+node.set['jenkins']['executor']['timeout'] = 7200 # wait up to 2 hours for jobs to complete
+node.run_state[:jenkins_private_key]       = delivery_bus_secrets['jenkins_private_key']
+
+node['build_cookbook']['toolchain_projects'].each do |toolchain_project|
+
+  #########################################################################
+  # Execute the build in Jenkins. The `jenkins_job` resource will block
+  # until completion AND stream back log output into the current CCR.
+  #########################################################################
+  jenkins_job toolchain_project do
+    parameters(
+      'GIT_REF' => node['delivery']['change']['sha'], # TODO: expose the change SHA in delivery-sugar,
+      'APPEND_TIMESTAMP' => 'false', # ensure we don't append a timestamp to our build version
+      'DELIVERY_CHANGE' => delivery_change_id,
+      'DELIVERY_SHA' => node['delivery']['change']['sha'],
+    )
+    action :build
+  end
+
+end
