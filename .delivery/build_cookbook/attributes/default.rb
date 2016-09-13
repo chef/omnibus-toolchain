e280aa4d83cbe44634768510e87580ed1f703753
Scott Hain <shain@chef.io>
2016-09-13 13:42:17 -0700

    Only build regular toolchain, the smart way

--- .delivery/build_cookbook/attributes/default.rb
+++ .delivery/build_cookbook/attributes/default.rb
@@ -1,6 +1,4 @@
 default['build_cookbook']['ruby_version']       = '2.3.0'
-# Need to find a way to parallelize these builds.
-# default['build_cookbook']['toolchain_projects'] = %w(
-#   angry-omnibus-toolchain
-#   omnibus-toolchain
-# )
+default['build_cookbook']['toolchain_projects'] = %w(
+  omnibus-toolchain
+)
