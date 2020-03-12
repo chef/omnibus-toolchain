1e4a8481f024aa01c6e875c3e509e1f332a02465
Jaymala Sinha <jsinha@chef.io>
2020-03-12 15:46:01 -0400

    Update various gem pinnings
    *  Remove bundler and rubygems dependencies as it now ships with ruby
    *  Update ruby version to 2.6.5
    *  Add dependency to ruby-cleanup to verify no double bundler
    
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

--- config/software/omnibus-toolchain.rb
+++ config/software/omnibus-toolchain.rb
@@ -38,8 +38,7 @@ dependency "cacerts"
 
 # ruby core tools
 dependency "ruby"
-dependency "rubygems"
-dependency "bundler"
+dependency "ruby-cleanup"
 
 if linux? || mac_os_x?
   dependency "berkshelf" unless i386? || ios_xr? || nexus? || armhf?
@@ -57,7 +56,7 @@ end
 # For Solaris 10 and Freebsd 9 we assume that you have installed the system gcc
 # package this means that pcre is going to link against it, and it's ok in this
 # case
-if solaris2? || (freebsd? && ohai["os_version"].to_i < 1000024)
+if solaris2?
   whitelist_file /libpcrecpp\.so\..+/
 end
 
