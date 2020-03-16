eda36f5f52744b5715c194ccfc8d6d48c9b984b9
Jaymala Sinha <jsinha@chef.io>
2020-03-16 10:25:00 -0400

    Override bundler version to install only one version
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

--- config/projects/omnibus-toolchain.rb
+++ config/projects/omnibus-toolchain.rb
@@ -37,6 +37,8 @@ else
 end
 
 override :ruby, version: "2.6.5"
+# pin same version of bundler that ships with ruby
+override :bundler,  version: "1.17.3"
 
 # tar 1.32 is not compatible with the Ubuntu 14.04's latest version of dpkg-deb so pin it to 1.28
 if ubuntu_trusty?
