5731a018bd4abde2c1c27ec97b83a7b35710ff38
Seth Chisamore <schisamo@chef.io>
2020-11-26 13:25:12 -0500

    Remove outdated references to nexus and ios_xr
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- config/software/gtar.rb
+++ config/software/gtar.rb
@@ -40,8 +40,7 @@ build do
     configure_command << " --without-selinux"
   end
 
-  if nexus? || ios_xr? || s390x?
-    # ios_xr and nexus don't support posix acls
+  if s390x?
     configure_command << " --without-posix-acls"
   elsif aix?
     if version.satisfies?("> 1.28") && version.satisfies?("< 1.32")
