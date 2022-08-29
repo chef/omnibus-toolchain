0a3162eaceb7bcaf1e0cfce693df7ae8f2659dd2
Poornima <poorndm@progress.com>
2022-08-29 12:13:24 +0530

    Update signing Identity
    Signed-off-by: Poornima <poorndm@progress.com>

--- config/projects/omnibus-toolchain.rb
+++ config/projects/omnibus-toolchain.rb
@@ -100,10 +100,10 @@ package :msi do
   upgrade_code msi_upgrade_code
   wix_candle_extension "WixUtilExtension"
   wix_light_extension "WixUtilExtension"
-  signing_identity "AF21BA8C9E50AE20DA9907B6E2D4B0CC3306CA03", machine_store: true
+  signing_identity "13B510D1CF1B3467856A064F1BEA12D0884D2528", machine_store: true
   parameters ProjectLocationDir: project_location_dir
 end
 
 package :appx do
-  signing_identity "AF21BA8C9E50AE20DA9907B6E2D4B0CC3306CA03", machine_store: true
+  signing_identity "13B510D1CF1B3467856A064F1BEA12D0884D2528", machine_store: true
 end
