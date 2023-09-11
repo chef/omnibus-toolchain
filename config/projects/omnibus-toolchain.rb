5d84bd390dcb2ad75c26c8a31f867a965234991d
Sean Simmons <ssimmons@progress.com>
2023-09-11 15:10:13 -0400

    machien store true was wrong, attempting with that set to false
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- config/projects/omnibus-toolchain.rb
+++ config/projects/omnibus-toolchain.rb
@@ -109,10 +109,10 @@ package :msi do
   upgrade_code msi_upgrade_code
   wix_candle_extension "WixUtilExtension"
   wix_light_extension "WixUtilExtension"
-  signing_identity "769E6AF679126F184850AAC7C5C823A80DB3ADAA", machine_store: true
+  signing_identity "769E6AF679126F184850AAC7C5C823A80DB3ADAA", machine_store: false
   parameters ProjectLocationDir: project_location_dir
 end
 
 package :appx do
-  signing_identity "769E6AF679126F184850AAC7C5C823A80DB3ADAA", machine_store: true
+  signing_identity "769E6AF679126F184850AAC7C5C823A80DB3ADAA", machine_store: false
 end
