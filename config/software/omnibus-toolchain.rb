bf7ad67b6a2511f0fc112270dcff5d59a79b3e58
muthuja <127753948+muthuja@users.noreply.github.com>
2025-03-20 18:10:44 +0530

    remove mac 11 as its EOL and update 12 as builder omniubs toolchain/muthuja (#292)
    * remove mac 11 and update 12 as builder
    
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>
    
    * pick latest sha of omnibus-buildkite-plugin
    
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>
    
    * stop builidng berkshelf for mac os as its not required on mac
    
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>
    
    * pick latest version of omnibus-software along with sha
    
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>
    
    * update the comments for reference
    
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>
    
    ---------
    
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- config/software/omnibus-toolchain.rb
+++ config/software/omnibus-toolchain.rb
@@ -41,7 +41,9 @@ dependency "ruby"
 
 # Projects such as Chef Server, Supermarket, Chef Manage and Chef Backend use berkshelf for the build time installation of gems.
 # Moving it to toolchain avoids installing a ton of deps into our packages.
-if linux? || macos?
+# chef-server and their components never built on mac , so we can stop building berkshelf on mac
+# if its requires in future for mac can add it back here and test
+if linux?
   dependency "berkshelf" unless i386? || arm?
 end
 
