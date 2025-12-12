567bc150d67aaf487cc59c8b0320379c9c5d57b0
muthuja <muthujabavaji.vempalli@progress.com>
2025-12-12 16:56:47 +0530

    install archive-tar-minitar to resolve the error
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

---
+++ config/software/berkshelf-no-depselector.rb
@@ -0,0 +1,49 @@
+
+#
+# Copyright 2016 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# expeditor/ignore: deprecated 2021-04
+
+name "berkshelf-no-depselector"
+default_version "main"
+
+license "Apache-2.0"
+license_file "LICENSE"
+
+source git: "https://github.com/berkshelf/berkshelf.git"
+
+relative_path "berkshelf"
+
+dependency "ruby"
+
+unless windows? && (project.overrides[:ruby].nil? || project.overrides[:ruby][:version] == "ruby-windows")
+  dependency "libarchive"
+end
+
+dependency "nokogiri"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  bundle "config set --local without guard changelog development test", env: env
+  bundle "install --jobs #{workers}", env: env
+
+  bundle "exec thor gem:build", env: env
+
+  gem "install pkg/berkshelf-*.gem" \
+      " --no-document", env: env
+  # This line ensures the missing runtime dep is present
+  gem "install archive-tar-minitar --no-document", env: env    
+end
